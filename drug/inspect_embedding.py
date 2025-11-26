4b9e1c72779d029e2de34d6a377a11447973328b
austinato <austinliuzirui@gmail.com>
2025-11-26 16:08:51 +0800

    fix split

--- drug/inspect_embedding.py
+++ drug/inspect_embedding.py
@@ -4,7 +4,7 @@ import numpy as np
 # this is data, i don't want to load as weights
 reps = torch.load("drug/embeddings_atomic/DB00130_unimol.pt", weights_only=False)
 print(reps.keys())
-print(np.array(reps["atomic_reprs"]).reshape(-1, 512))  # (N_atoms, 512)
-print(np.array(reps["atomic_reprs"]).reshape(-1, 512).shape)  # (N_atoms, 512)
-print(np.array(reps["atomic_symbol"]))  # (N_atoms, 512)
-print(np.array(reps["atomic_symbol"]).shape)  # (N_atoms, 512)
+print(np.array(reps["atomic_reprs"])[:,:-1,:])  # (N_atoms, 512)
+print(np.array(reps["atomic_reprs"])[:,:-1,:].shape)  # (N_atoms, 512)
+print(np.array(reps["atomic_symbol"])[:,:-1])  # (N_atoms, 512)
+print(np.array(reps["atomic_symbol"])[:,:-1].shape)  # (N_atoms, 512)
